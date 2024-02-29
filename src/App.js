61f3168732e0788291c10933032f9f6da810c261
Shivangi Srivastava <shi06dec@gmail.com>
2024-02-29 16:56:08 -0700

    Create book functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import { useState } from 'react';
+import BookCreate from './components/BookCreate';
 import './App.css';
 
 function App() {
   const [books, setBooks] = useState([]);
-  
+
+  const createBook = (title) => {
+    console.log("Need to add book with title:", title);
+  }
+
   return (
-    <div>App</div>
+    <div>
+      <BookCreate onCreate={createBook} />
+    </div>
   );
 }
 
--- src/components/BookCreate.js
+++ src/components/BookCreate.js
@@ -1,6 +1,26 @@
-function BookCreate() {
+import { useState } from "react";
+
+function BookCreate({ onCreate }) {
+  const [title, setTitle] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    onCreate(title);
+    setTitle('');
+  }
+
+  const handleChange = (event) => {
+    setTitle(event.target.value);
+  }
+
   return (
-    <div>Book Create</div>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title</label>
+        <input type="text" name="title" value={title} onChange={handleChange} />
+        <button>Create book!</button>
+        </form>
+    </div>
   );
 }
 
