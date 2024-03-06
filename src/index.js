833137d1d259a2adf2e2d28c17af1696af8c9607
Shivangi Srivastava <shi06dec@gmail.com>
2024-03-05 17:21:58 -0700

    create context, share the value 5 using provider and use that value within a specific component using useContext

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import BooksContext from './context/books';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BooksContext.Provider value={5}>
+      <App />
+    </BooksContext.Provider>
   </React.StrictMode>
 );
