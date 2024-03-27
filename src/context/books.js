8b52ab18f6b71056b3ec8ea413d5905f807efc85
Shivangi Srivastava <shi06dec@gmail.com>
2024-03-27 14:04:42 -0600

    Fix bugs with useCallback

--- src/context/books.js
+++ src/context/books.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useCallback } from "react";
 import axios from "axios";
 
 const BooksContext = createContext();
@@ -6,12 +6,12 @@ const BooksContext = createContext();
 function Provider({ children }) {
   const [books, setBooks] = useState([]);
 
-  const fetchBooks = async () => {
+  const fetchBooks = useCallback(async () => {
     const response = await axios.get('http://localhost:3001/books');
     console.log(response);
 
     setBooks(response.data);
-  };
+  }, []);
 
   const deleteBookById = async (id) => {
     await axios.delete(`http://localhost:3001/books/${id}`);
